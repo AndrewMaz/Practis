8e8b9dfab886bda520169dcb07c02133dcb3b672
DESKTOP-6L3AKJ7\mazur <mazurenko_an@mail.ru>
2021-02-15 16:41:13 +0300

    Прокси

---
+++ Proxy.Coding.Exercise/Proxy.Coding.Exercise/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Proxy.Coding.Exercise
+{
+    class Program
+    {
+        public class Person
+        {
+            public int Age { get; set; }
+
+            public string Drink()
+            {
+                return "drinking";
+            }
+
+            public string Drive()
+            {
+                return "driving";
+            }
+
+            public string DrinkAndDrive()
+            {
+                return "driving while drunk";
+            }
+        }
+
+        public class ResponsiblePerson
+        {
+            Person person;
+            public ResponsiblePerson(Person person)
+            {
+                this.person = person;
+            }
+
+            public string Drink()
+            {
+                if (person.Age >= 18)
+                    return person.Drink();
+                return ("too Young");
+            }
+
+            public string Drive()
+            {
+                if (person.Age >= 16)
+                    return person.Drive();
+                return "too young";
+            }
+
+            public string DrinkAndDrive()
+            {
+                return "dead";
+            }
+            public int Age { get { return person.Age; } }
+        }
+        static void Main(string[] args)
+        {
+            var rp1 = new ResponsiblePerson(new Person { Age = 18 });
+            Console.WriteLine(rp1.Drink());
+            Console.WriteLine(rp1.Drive()); 
+            Console.WriteLine(rp1.DrinkAndDrive() + "\n");
+
+            var rp2 = new ResponsiblePerson(new Person { Age = 14 });
+            Console.WriteLine(rp2.Drink());
+            Console.WriteLine(rp2.Drive());
+            Console.WriteLine(rp2.DrinkAndDrive());
+        }
+    }
+}
