2290848c76c70d60543fbd86ad97f930749add4b
DESKTOP-6L3AKJ7\mazur <mazurenko_an@mail.ru>
2021-02-17 20:48:36 +0300

    Null Object

---
+++ NullObject.Coding.Exercise/NullObject.Coding.Exercise/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace NullObject.Coding.Exercise
+{
+    public interface ILog
+    {
+        // maximum # of elements in the log
+        int RecordLimit { get; }
+
+        // number of elements already in the log
+        int RecordCount { get; set; }
+
+        // expected to increment RecordCount
+        void LogInfo(string message);
+    }
+
+    public class Account
+    {
+        private ILog log;
+
+        public Account(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void SomeOperation()
+        {
+            int c = log.RecordCount;
+            log.LogInfo("Performing an operation");
+            if (c + 1 != log.RecordCount)
+                throw new Exception();
+            if (log.RecordCount >= log.RecordLimit)
+                throw new Exception();
+        }
+    }
+
+    public class NullLog : ILog
+    {
+        int count;
+        public int RecordLimit => RecordCount + 1;
+        public int RecordCount { get { return count; } set { count = RecordCount; } }
+        public void LogInfo(string message) { count++; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nullObj = new NullLog();
+            var ac = new Account(nullObj);
+
+            ac.SomeOperation();
+
+        }
+    }
+}
